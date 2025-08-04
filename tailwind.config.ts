4e22d46c7f5fe798ba4d615bdbea6f1f4c98bc22
sivasathish889 <sivasathish889@gmail.com>
2025-08-04 18:56:36 +0530

    build error

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,6 +3,7 @@ import type { Config } from "tailwindcss";
 const config: Config = {
   content: [
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
